080c356525a3c40d72200281088813c9f0d0fe4f
muhammadasruls <43999703+muhammadasruls@users.noreply.github.com>
2018-10-10 00:11:02 +0800

    Add files via upload

---
+++ NODE.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author MUH ASRUL S
+ */
+class node {
+    String Label;
+    int Value;
+
+    void setLabel(String label) {
+        Label = label;
+    }
+
+    public String getLabel() {
+        return Label;
+    }
+
+    void setValue(int value) {
+        Value = value;
+    }
+
+    public int getValue() {
+        return Value;
+    }
+}
+
---
+++ STCK.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author MUH ASRUL S
+ */
+class stack {
+    String[] value = new String[6];
+
+    void setValueAt(int position, String value){
+
+    }
+}
